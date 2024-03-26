4e348d47c0aab7452e8a8942caa530b0e2c3edb0
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-26 11:41:22 +0100

    Update largest_squared.py

--- Short Coding Problems/largest_squared.py
+++ Short Coding Problems/largest_squared.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def find_largest_square(arr):
+def find_largest_squared(arr):
     largest = arr[0]
     for i in arr:
         if i > largest:
             largest = i
-    return largest **2
+    return largest, largest **2
 
-array = np.random.randint(0, 1000, 10)
-largest_squared_number = find_largest_square(array)
-print(f"The largest squared number in the list is {largest_squared_number}")
+array = np.random.randint(0, 1000, 20)
+largest, squared = find_largest_squared(array)
+print(f"The largest number in the array is {largest}, and its square is {squared}.")
