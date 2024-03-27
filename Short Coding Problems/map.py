d2015098ac9691ca0e166e8819344441bf0d944d
Julie Le Rudulier <136101496+julielerudulier@users.noreply.github.com>
2024-03-27 14:16:26 +0100

    Update map.py

--- Short Coding Problems/map.py
+++ Short Coding Problems/map.py
@@ -1,5 +1,5 @@
 # 1st example: Applying a function to a single iterable
-# Define a function that returns an integer's square
+# Define a function that returns the square of an integer
 def square(n):
     return n ** 2
 
@@ -34,7 +34,7 @@ print(result)
 
 # ---
 
-# 3rd example: Lambda Functions and map()
+# 3rd example: Lambda functions and map()
 # Following up on example #1, we can use a lambda function to square each element in a list and obtain the same result as with a function
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = list(map(lambda x: x ** 2, numbers))
@@ -44,32 +44,19 @@ print(squared_numbers)
 
 # ---
 
+# 4th example: Combining map() with other functions
+# In this example, we will use map() in conjunction with filter() to filter elements from a list based on a certain condition
+# Define a function that returns the square of a given integer
+def square(n):
+    return n ** 2
 
+# Define a second function that returns where an integer is odd or not
+def is_odd(n):
+    return n % 2 == 1
 
+# Create a list of integers
+numbers = [2, 1, 3, 4, 7, 11, 18]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
+# Create a list of squared odd numbers
+result = list(map(square, filter(is_odd, numbers)))
+print(result)
